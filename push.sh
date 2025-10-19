3428d848fcedb0fe9070999b6aa6ee49057e5efa
Maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-10-19 18:46:58 +0700

    feat(model): add Category model and link it to Course \n     BREAKING CHANGE: Course now requires a category_id field.

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
     git add .
-    git commit -m "feat(model): add Category model and link it to Course
+    git commit -m "feat(model): add Category model and link it to Course \n
     BREAKING CHANGE: Course now requires a category_id field."
     git push origin main
